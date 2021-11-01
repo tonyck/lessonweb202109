ecf18067381e5c919d7411babca36c31629960bb
tonyck <tony0912@gmail.com>
2021-11-01 23:16:31 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -7,9 +7,10 @@ from flask import Flask, request, abort
 
 app = Flask(__name__)
 
+
 @app.route("/", methods=['GET'])
 def basic_url():
-    return 'hello'
+    return '<h1>賴田捕手第 20 天</h1><p>全身都是肌肉沒半點腦子。反正，那就是鄭尼那晚照顧的草泥馬。我完全無法了解，我發誓我沒辦法。</p>'
 
 
 if __name__ == "__main__":
