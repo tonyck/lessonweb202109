3b42ecfc3694c5dcb4e44ea819116f0cc6e8bf3b
tonyck <tony0912@gmail.com>
2021-11-01 22:41:12 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -1,12 +1,17 @@
 # -*- coding: utf-8 -*-
-from flask_ngrok import run_with_ngrok
-from flask import Flask
-
-app = Flask(__name__)  # _name_ 代表目前執行的模組
-run_with_ngrok(app)  # starts ngrok when the app is run
 
 
-@app.route("/")  # 函式的裝飾(Decorator)：以函式為基礎，提供附加的功能
-# 撰寫程式
-def home():
-    return "<h1>Running Flask on Google Colab!</h1>"
+from __future__ import unicode_literals
+
+from flask import Flask, request, abort
+
+app = Flask(__name__)
+
+
+@app.route("/", methods=['GET'])
+def basic_url():
+    return 'hello'
+
+
+if __name__ == "__main__":
+    app.run()
